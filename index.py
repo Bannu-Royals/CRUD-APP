463c02ff2ca8fa349a77e51905c582c49a2b7c75
Durga Prasad Kakileti <durgaprasadkakileti@gmail.com>
2024-07-16 20:20:35 +0530

    updated

--- index.py
+++ index.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 # Use pymysql as the MySQL connector
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = os.environ.get('SECRET_KEY')
+# app.secret_key = os.environ.get('SECRET_KEY')
 
 db = SQLAlchemy(app)
 
